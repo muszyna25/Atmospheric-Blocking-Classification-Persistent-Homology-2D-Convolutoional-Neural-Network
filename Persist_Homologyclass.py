normalization fixed

Persist_Homologyclass.py
@@ -78,6 +78,10 @@ class Persist_Homologyclass(object):
         img = preprocessing.normalize(img)
         scaler = preprocessing.StandardScaler()
         img = scaler.fit_transform(img)
+        
+        #X = img - np.mean(img, axis=0)
+        #img = X/np.std(img, axis=0)
+        
         return img
 
 #............................
@@ -195,11 +199,13 @@ class Persist_Homologyclass(object):
                 print('Timestep: %d' % j)
                 img = fd[j] #Gets an image from a file.
                 self.l_globalimgs.append(img)
-                #img = self.preprocessing_norm_stand(img) #Normalizes & standardizes data.
-                self.l_imgs.extend(self.extract_subimages(img, 4)) #Extracts eight subimages.
+                #img = self.preprocessing_norm_stand(img) #Normalizes & standardizes data..
+                imgs = self.extract_subimages(img, 4)
+                self.l_imgs.extend(imgs) #Extracts eight subimages.
                 for k in range(0, len(self.l_imgs)):
                     #if k % 2 == 0: #I = add_rnd_noise(l_imgs[k]) #Adds randomness to create second class of objects in the input raw data.
                     I = self.l_imgs[k]
+                    I = self.preprocessing_norm_stand(I) #Normalizes & standardizes data.
                     self.dgms = self.PH_func_call(I, self.norm, self.maxdim) #Computes H1 homologies.
                     self.l_dgms.append(self.dgms)
                     new_repres_img = self.hist_data(self.dgms) #Computes 2d histogram.

Plotter_Func2class.py
@@ -80,11 +80,12 @@ class Plotter_Func2class(object):
 #####........My code....................    
 
 #............................
-    def plot_multiple_diagrams(self, dim, l_dgms, idxL, figId=13):
+    def plot_multiple_diagrams(self, dim, ph, idxL, figId=21):
         figId = self.smart_append(figId)
         fig = self.plt.figure(figId, facecolor='white', figsize=(8,8))
         j=0; nrow, ncol=int(len(idxL)/2),2
         fig.suptitle('Diagrams: H%i' %dim, fontsize=12)
+        l_dgms = ph.l_dgms # Get list of persistence diagrams (barcodes) from the object class. 
 
         for i in idxL:
             if dim == 0: 
@@ -111,11 +112,12 @@ class Plotter_Func2class(object):
             j+=1
         
 #............................
-    def plot_multiple_barcodes(self, dim, l_dgms, idxL, figId=12):
+    def plot_multiple_barcodes(self, dim, ph, idxL, figId=22):
         figId = self.smart_append(figId)
         fig = self.plt.figure(figId, facecolor='white', figsize=(8,8))
         j=0; nrow, ncol=int(len(idxL)/2),2
         fig.suptitle('Barcodes: H%i' %dim, fontsize=12)
+        l_dgms = ph.l_dgms # Get list of persistence diagrams (barcodes) from the object class. 
 
         for i in idxL:
             if dim == 0: 
@@ -127,8 +129,6 @@ class Plotter_Func2class(object):
 
             offset = 0 #Sets the vertical offset between bars (lines) for persistence barcode plot.
             no = len(data) #Gets number of points (lines/bars).
-            #print('Number of bars in H0: %i' %no)
-
             #Plot persistence barcode.
             ax = self.plt.subplot(nrow, ncol, j+1)
             ax.autoscale(enable=True) #Adjusts the scale of axes automatically.
@@ -136,12 +136,12 @@ class Plotter_Func2class(object):
                 ax.hlines(y=0.1+offset, xmin=data[i][0], xmax=data[i][1], linestyle='-', linewidth=1, color='k') #Plots all horizontal lines/bars.
                 offset += 0.09 #Shifts each line/bar by the fixed offset.
         
-            #self.plt.title('Barcode: H%i | No of bars: %i' %(dim, no))
             self.plt.xlabel('Threshold (parameter)')
             self.plt.ylabel('Betti number')
             self.plt.yticks([]) #Sets no ticks on y-axis.
             self.plt.grid(True) #Sets grid on.
-            ax.set_title('%i (No. of bars: %i)' %(j,no))
+            ax.set_title('ID: %i; Class: %i; (No. of bars: %i)' %(j, 000, no))
+            self.plt.xlim(0,1)
             j+=1
         
 #............................
@@ -197,10 +197,10 @@ class Plotter_Func2class(object):
 
 #............................
     def plot_multiple_imgs(self, ph, idxL, figId=25):
-        figId=self.smart_append(figId)
-        fig=self.plt.figure(figId, facecolor='white', figsize=(8,8))
-        n=len(ph.l_imgs); #no_imgs = int(np.round(n/2));
-        j=0; nrow,ncol=int(len(idxL)/2),2
+        figId = self.smart_append(figId)
+        fig = self.plt.figure(figId, facecolor='white', figsize=(8,8))
+        n = len(ph.l_imgs); #no_imgs = int(np.round(n/2));
+        j = 0; nrow,ncol=int(len(idxL)/2),2
         fig.suptitle('Extracted subimages' + '\n' + 'Variable name: %s, unit: %s' %(ph.varname, self.dict_units[ph.varname]), fontsize=7)
         for i in idxL: 
             ax = self.plt.subplot(nrow, ncol, j+1)

format_Func2class.py
@@ -53,36 +53,37 @@ ecmwf_data_path='/global/cscratch1/sd/muszyng/ethz_data/ecmwf_data/' # Name of o
 varname='pv'
 ecmwf_labeled_data_path='/global/cscratch1/sd/muszyng/ethz_data/labels/'
 
+'''These two are for testing'''
+#out=ph.read_netcdf_file_('/global/cscratch1/sd/muszyng/ethz_data/ecmwf_download/batch_scripts/', 'ERA_INTERIM_1979.nc', 'pv') #Variables names: e.g., 'lon', 'lat', 'prw'
+#outputListData=[np.random.random((100, 100)) for i in range(1888)] # List of fake 2d histograms (10x10). 
+
 #............................
 ph=Persist_Homologyclass(ecmwf_data_path, ecmwf_labeled_data_path, varname) # You can specify max homology dimension and metric for distance matrix.
 
 ph.generate_list_of_files()
 ph.generate_data_list()
-#ph.generate_labeled_data_list()
-#print(ph.Y_labels)
-
-#out=ph.read_netcdf_file_('/global/cscratch1/sd/muszyng/ethz_data/ecmwf_download/batch_scripts/', 'ERA_INTERIM_1979.nc', 'pv') #Variables names: e.g., 'lon', 'lat', 'prw'
-#print(out.shape)
+ph.generate_labeled_data_list()
 
 #............................
-#outputListData=[np.random.random((100, 100)) for i in range(1888)] # List of fake 2d histograms (10x10). 
+'''These two need to be fixed'''
 #ph.save_dict_to_hdf5(ph.outputData)
 #ph.load_hdf5_file('train.hd5')
+
+''' Fixed plotting global image and subimages
 gra.plot_global_img(ph, 0)
 gra.plot_multiple_imgs(ph, range(8))
+'''
 
 ''' Fixed plotting histograms
 gra.plot_multiple_1d_histograms(ph, range(8)) 
 gra.plot_multiple_2d_histograms(ph, range(8)) 
 '''
 
-#gra.plot_multiple_barcodes(0, ph.l_dgms, range(8))
-#gra.plot_multiple_diagrams(1, ph.l_dgms, range(8))
-
-#print(ph.l_globalimgs[0].shape)
-#gra.plot_global_img(ph.labels_l_globalimgs, 0)
-#print(ph.labels_l_globalimgs[0].shape)
-#gra.plot_multiple_imgs(ph.labels_l_imgs, range(8))
+''' Fixed plotting barcodes and diagrams
+gra.plot_multiple_barcodes(0, ph, range(8))
+gra.plot_multiple_barcodes(1, ph, range(8))
+gra.plot_multiple_diagrams(1, ph, range(8))
+'''
 
 gra.display_all('form') #This is to plot all collected figures.
 