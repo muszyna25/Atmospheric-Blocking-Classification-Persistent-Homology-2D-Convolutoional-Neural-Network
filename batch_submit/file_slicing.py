033240226a5ef438f4a35119e2dfa4b4f0da6df4
muszyna25 <muszyna23@gmail.com>
2019-04-09 12:51:38 -0700

    File slicing complete.

--- batch_submit/file_slicing.py
+++ batch_submit/file_slicing.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
-import os
 from netCDF4 import Dataset
 import numpy as np
 import sys
 
 #............................
-    def read_netcdf_file(fname, varname): #Variables names: e.g., 'lon', 'lat', 'prw'
+def read_netcdf_file(fname, varname): # Variables names: e.g., 'lon', 'lat', 'prw'
         fh = Dataset(fname, mode='r')
-        var_netcdf = fh.variables[varname][:] #Retrieves a given variable by name.
+        var_netcdf = fh.variables[varname][:] # Retrieves a given variable by name.
         fh.close()
         return var_netcdf
 
@@ -18,9 +17,22 @@ if __name__ == "__main__":
     f_idx = int(sys.argv[2]) # SLURM_ARRAY_TASK_ID
     n_proc = int(sys.argv[3])
 
-    data = read_netcdf_file(fname, 't')
+    print('[+] ==== %s File name: %s File index: %i No processes/frames: %i' %(sys.argv[0], fname, f_idx, n_proc))
 
-    f_data = [data[int(i + f_idx*32)][0] for i in range(0, n_proc) if int(i + f_idx*32) <= 1459]
+    data = read_netcdf_file(fname, 't') # For now fix the variable.
 
-    np.savez(fname.split('/')[-1][:-3] + '_' + str(f_idx) + '_' + '.npz', *f_data) # Pattern: ERA_INTERIM_1980_(f_idx)_.npz
+    f_data = [data[int(i + f_idx*32)][0] for i in range(0, n_proc) if int(i + f_idx*32) <= 1459] # Last slice of frames has 20 frames.
+
+    if len(f_data) == 0: # Safely exit if all indices are out of file range.
+        print('[-] ==== SAFE EXIT! Indices out of file range.')
+        sys.exit(0)
+
+    print('==== Min frame index: %d Max frame index: %d ' %(f_idx*32, (n_proc-1)+f_idx*32))
+    print('==== Extracted number of frames: %d ' %len(f_data))
+    print('==== Size of one frame: %d %d ' %(f_data[-1].shape[0], f_data[-1].shape[1]))
+
+    new_fname = fname.split('/')[-1][:-3] + '_' + str(f_idx) + '_' + '.npz'
+    np.savez(new_fname, *f_data) # Pattern: ERA_INTERIM_1980_(f_idx)_.npz
+
+    print('[+] ==== File: ' + new_fname + ' saved!')
 
